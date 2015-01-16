3b227fdbf9fce4cb46f2826c56e1cd3efa01352b
Lucarnosky <lucarnosky@gmail.com>
2015-01-16 18:43:39 +0100

    Added some function on Vector2D

--- src/garnocktoolkit/lucarnosky/physics/Vector2d.java
+++ src/garnocktoolkit/lucarnosky/physics/Vector2d.java
@@ -7,7 +7,7 @@ public class Vector2d {
 	/**
 	 * Attempt to have a full working custom Vector System
 	 * @author Matteo Lucarno
-	 * @version 1.0.0
+	 * @version 1.0.1
 	 */
 	
 	/**Vector Axis*/
@@ -53,7 +53,7 @@ public class Vector2d {
 	
 	/**
 	*	This function provide a vector addition
-	*	@param vectorToAdd the vector2d to add at the current vector
+	*	@param Vector2d vectorToAdd the vector2d to add at the current vector
 	*/
 	public void addToVector2d(Vector2d vectorToAdd){
 		x += vectorToAdd.x;
@@ -62,7 +62,7 @@ public class Vector2d {
 	
 	/**
 	 * This function provide the intensity of the vector
-	 * @return the intensity of the vector2D
+	 * @return double the intensity of the vector2D
 	 */
 	public double getIntensity(){
 		return Math.sqrt( Math.pow(x, 2) + Math.pow(y, 2));
@@ -72,12 +72,35 @@ public class Vector2d {
 	 * This function returns the direction of the vector
 	 * It should be read in degree 
 	 * 
-	 * @return the direction in degree
+	 * @return double the direction in degree
 	 */
 	public double getDirection(){
 		return Math.tan(x/y);
 	}
 	
+	/**
+	 * This function provide the pointing way of the vector2D
+	 * It just help to realize how to rapresent the vector on a cartesian plane
+	 * @return Point the x and y direction should read just the sign of the couple
+	 */
+	public Point getPointingDirection(){
+		double px, py;
+		if(x < 0){
+			px = -1;
+		}else if (x > 0){
+			px = 1;
+		}else{
+			px = 0;
+		}
+		if(y < 0){
+			py = -1;
+		}else if (y > 0){
+			py = 1;
+		}else{
+			py = 0;
+		}
+		return new Point((int)px,(int)py);
+	}
 	
 }
 
